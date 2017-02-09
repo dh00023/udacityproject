2395298cdced16ebc8d9870bdd979d85de4f862c
Dahye Jeong <wjdekgp1750@naver.com>
2017-02-10 03:34:56 +0900

    header.js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -2,7 +2,13 @@
 This is empty on purpose! Your code to build the resume will go here.
  */
 // $("#main").append(["Dahye Jeong"]);
+/*
 var awesomeThoughts="I am Dahye and I am AWESOME!";
 console.log(awesomeThoughts);
 var funThoughts=awesomeThoughts.replace("AWESOME","FUN");
 $("#main").append(funThoughts);
+*/
+var formatteName = HTMLheaderName.replace("%data%","Dahye Jeong");
+$("#header").append(formatteName);
+var formattedRole = HTMLheaderRole.replace("%data%","front-end developer");
+$("#header").append(formattedRole);
