9bf44eb1a2fc5213e8992b5ad5115f52562bbd22
Dahye Jeong <wjdekgp1750@naver.com>
2017-02-10 03:13:38 +0900

    var,replace

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,4 +1,8 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
- $("#main").append(["Dahye Jeong"]);
+// $("#main").append(["Dahye Jeong"]);
+var awesomeThoughts="I am Dahye and I am AWESOME!";
+console.log(awesomeThoughts);
+var funThoughts=awesomeThoughts.replace("AWESOME","FUN");
+$("#main").append(funThoughts);
