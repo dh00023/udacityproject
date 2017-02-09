d6c33f8b19096275807973b6895cbb728b3d53dc
Dahye Jeong <wjdekgp1750@naver.com>
2017-02-10 04:51:24 +0900

    object

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -8,7 +8,77 @@ console.log(awesomeThoughts);
 var funThoughts=awesomeThoughts.replace("AWESOME","FUN");
 $("#main").append(funThoughts);
 */
-var formatteName = HTMLheaderName.replace("%data%","Dahye Jeong");
+/*
 $("#header").append(formatteName);
 var formattedRole = HTMLheaderRole.replace("%data%","front-end developer");
 $("#header").append(formattedRole);
+*/
+//Object
+var bio={
+	"name" : "Dahye Jeong",
+	"role" : "Student",
+	"skills":["html","css","ROR","js"],
+	"Contact" : {
+		"email" : "asdfa0@email.com",
+		"mobile" : "010-xxxx-xxxx",
+		"github" : "dh00023",
+		"twitter" : "asdfasdf",
+		"location" : "seoul"
+	},
+	"welcomeMessage": "Hello, Welcome to my page",
+	"biopic": "images/fry.jpg"
+}
+var education={
+	"school": [{
+		"name": "konkuk",
+		"location": "seoul",
+		"major": ["statics", "computer science"],
+		"graduation": 2018,
+		"url": "http://likelion.net"
+	}, {
+		"name": "seoul",
+		"location": "seoul",
+		"major": ["statics", "computer science"],
+		"graduation": 2018,
+		"url": "http://likelion.net"
+	}],
+	"onlineCourses": [{
+		"title": "front-end nanodegree",
+		"school": "konkuk",
+		"dates": "2017-5",
+		"url": "https://www.udacity.com/"
+	}, {
+		"title": "fullstack nanodegree",
+		"school": "konkuk",
+		"dates": "2017-5",
+		"url": "https://www.udacity.com/"
+	}]
+}
+var work = {
+	"jobs": [{
+		"employer": "front-end",
+		"title": "udacity",
+		"location": "America",
+		"dates": "2017-3",
+		"description": "asdfasdfasdfasdfasdf"
+	}, {
+		"employer": "bacl-end",
+		"title": "udacity",
+		"location": "America",
+		"dates": "2017-3",
+		"description": "asdfasdfasdfasdfasdf"
+	}]
+}
+var projects={
+	"projects": [{
+		"title": "make a website",
+		"dates": "2017-04",
+		"description": "sdjfkljasdkfjlaskdf",
+		"images": "images/197x148.gif"
+	}, {
+		"title": "make a portfolio",
+		"dates": "2017-05",
+		"description": "sdjfkljasdkfjlaskdf",
+		"images": "images/197x148.gif"
+	}]
+}
