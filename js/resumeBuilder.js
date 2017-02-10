456e6216094c21bac5c6b7957fe26c74be0e7e1d
Dahye Jeong <wjdekgp1750@naver.com>
2017-02-11 03:29:36 +0900

    Final project

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -8,22 +8,18 @@ console.log(awesomeThoughts);
 var funThoughts=awesomeThoughts.replace("AWESOME","FUN");
 $("#main").append(funThoughts);
 */
-var formattedName=HTMLheaderName.replace("%data%","Dahye Jeong");
-$("#header").append(formattedName);
-var formattedRole = HTMLheaderRole.replace("%data%","front-end developer");
-$("#header").append(formattedRole);
 
 //Object
 var bio={
 	"name" : "Dahye Jeong",
 	"role" : "Student",
 	"skills":["html","css","ROR","js"],
-	"Contact" : {
+	"Contacts" : {
 		"email" : "asdfa0@email.com",
 		"mobile" : "010-xxxx-xxxx",
 		"github" : "dh00023",
-		"twitter" : "asdfasdf",
-		"location" : "Seoul, SouthKorea"
+		"twitter" : "@asdfasdf",
+		"location" : "Edmonton, Alberta, Canada"
 	},
 	"welcomeMessage": "Hello, Welcome to my page",
 	"biopic": "images/fry.jpg"
@@ -31,16 +27,16 @@ var bio={
 var education={
 	"school": [{
 		"name": "konkuk",
-		"location": "seoul",
+		"location": "Nashville, TN",
 		"major": ["statics", "computer science"],
 		"graduation": 2018,
-		"url": "http://likelion.net"
+		"degree": "석사"
 	}, {
 		"name": "seoul",
-		"location": "seoul",
+		"location": "Nashville, TN",
 		"major": ["statics", "computer science"],
 		"graduation": 2018,
-		"url": "http://likelion.net"
+		"degree": "university"
 	}],
 	"onlineCourses": [{
 		"title": "front-end nanodegree",
@@ -92,26 +88,41 @@ var projects={
 		"title": "make a website",
 		"dates": "2017-04",
 		"description": "sdjfkljasdkfjlaskdf",
-		"images": "images/197x148.gif"
-	}, {
-		"title": "make a portfolio",
-		"dates": "2017-05",
-		"description": "sdjfkljasdkfjlaskdf",
-		"images": "images/197x148.gif"
+		"images": ["images/197x148.gif","images/197x148.gif"]
 	}]
 }
-//if
-if (bio.skills.length > 0) {
-	$("#header").append(HTMLskillsStart);
+bio.display=function(){
+	var formattedRole = HTMLheaderRole.replace("%data%","front-end developer");
+	$("#header").prepend(formattedRole);
+	var formattedName=HTMLheaderName.replace("%data%","Dahye Jeong");
+	$("#header").prepend(formattedName);
+	var formattedMobile=HTMLmobile.replace("%data%",bio.Contacts.mobile);
+	$("#topContacts").append(formattedMobile);
+	var formattedEmail=HTMLemail.replace("%data%",bio.Contacts.email);
+	$("#topContacts").append(formattedEmail);
+	var formattedGithub=HTMLgithub.replace("%data%",bio.Contacts.github);
+	$("#topContacts").append(formattedGithub);
+	var formattedTwitter=HTMLtwitter.replace("%data%",bio.Contacts.twitter);
+	$("#topContacts").append(formattedTwitter);
+	var formattedLocation=HTMLlocation.replace("%data%",bio.Contacts.location);
+	$("#topContacts").append(formattedLocation);
+	var formattedImages=HTMLbioPic.replace("%data%",bio.biopic);
+	$("#header").append(formattedImages);
 
-	var formattedSkill=HTMLskills.replace("%data%",bio.skills[0]);
-	$("#skills").append(formattedSkill);
-	formattedSkill=HTMLskills.replace("%data%",bio.skills[1]);
-	$("#skills").append(formattedSkill);
-	formattedSkill=HTMLskills.replace("%data%",bio.skills[2]);
-	$("#skills").append(formattedSkill);
-	formattedSkill=HTMLskills.replace("%data%",bio.skills[3]);
-	$("#skills").append(formattedSkill);
+
+	//if
+	if (bio.skills.length > 0) {
+		$("#header").append(HTMLskillsStart);
+
+		var formattedSkill=HTMLskills.replace("%data%",bio.skills[0]);
+		$("#skills").append(formattedSkill);
+		formattedSkill=HTMLskills.replace("%data%",bio.skills[1]);
+		$("#skills").append(formattedSkill);
+		formattedSkill=HTMLskills.replace("%data%",bio.skills[2]);
+		$("#skills").append(formattedSkill);
+		formattedSkill=HTMLskills.replace("%data%",bio.skills[3]);
+		$("#skills").append(formattedSkill);
+	}
 }
 //for in,function
 //javascript에서 for-in은 안좋음!사용하지말자.for문을사용해!
@@ -133,12 +144,8 @@ function displaywork(){
 
 		var formattedDescription=HTMLworkDescription.replace("%data%",work.jobs[job].description);
 		$(".work-entry:last").append(formattedDescription);
-
-
 	}
 }
-displaywork();
-/*
 //Click해서 x와 y좌표 출력하기.
 
 $(document).click(function(loc) {
@@ -148,7 +155,7 @@ $(document).click(function(loc) {
 
   logClicks(x,y);
 });
-*/
+
 //button
 function inName(name){
 	name=name.trim().split(" ");
@@ -168,10 +175,56 @@ projects.display=function(){
 		$(".project-entry:last").append(formattedDates);
 		var formattedDescription=HTMLprojectDescription.replace("%data%",projects.projects[project].description);
 		$(".project-entry:last").append(formattedDescription);
-		var formattedImages=HTMLprojectImage.replace("%data%",projects.projects[project].images);
+		var formattedImages=HTMLprojectImage.replace("%data%",projects.projects[project].images[0]);
 		$(".project-entry:last").append(formattedImages);
+		var formattedImages=HTMLprojectImage.replace("%data%",projects.projects[project].images[1]);
+	}	$(".project-entry:last").append(formattedImages);
+}
+education.display=function(){
+	$("#education").append(HTMLschoolStart);
+		
+	for(e in education.school){
+		
+		var formattedName=HTMLschoolName.replace("%data%",education.school[e].name);
+		$(".education-entry:last").append(formattedName);
+		var formattedDegree=HTMLschoolDegree.replace("%data%",education.school[e].degree);
+		$(".education-entry:last").append(formattedDegree);
+		var formattedDates=HTMLschoolDates.replace("%data%",education.school[e].graduation);
+		$(".education-entry:last").append(formattedDates);
+		var formattedLocation=HTMLschoolLocation.replace("%data%",education.school[e].location);
+		$(".education-entry:last").append(formattedLocation);
+		var formattedMajor=HTMLschoolMajor.replace("%data%",education.school[e].major);
+		$(".education-entry:last").append(formattedMajor);
+	}
+
+	$(".education-entry").append(HTMLonlineClasses);
+		
+	for(ed in education.onlineCourses){
+			
+		var formattedTitle=HTMLonlineTitle.replace("%data%",education.onlineCourses[ed].title);
+		$(".education-entry:last").append(formattedTitle);
+		var formattedSchool=HTMLonlineSchool.replace("%data%",education.onlineCourses[ed].school);
+		$(".education-entry:last").append(formattedSchool);
+		var formattedDates=HTMLonlineDates.replace("%data%",education.onlineCourses[ed].dates);
+		$(".education-entry:last").append(formattedDates);
+		var formattedURL=HTMLonlineURL.replace("%data%",education.onlineCourses[ed].url);
+		$(".education-entry:last").append(formattedURL);
+		
 	}
 }
+	var formattedMobile=HTMLmobile.replace("%data%",bio.Contacts.mobile);
+	$("#footerContacts").append(formattedMobile);
+	var formattedEmail=HTMLemail.replace("%data%",bio.Contacts.email);
+	$("#footerContacts").append(formattedEmail);
+	var formattedGithub=HTMLgithub.replace("%data%",bio.Contacts.github);
+	$("#footerContacts").append(formattedGithub);
+	var formattedTwitter=HTMLtwitter.replace("%data%",bio.Contacts.twitter);
+	$("#footerContacts").append(formattedTwitter);
+	var formattedLocation=HTMLlocation.replace("%data%",bio.Contacts.location);
+	$("#footerContacts").append(formattedLocation);
+bio.display();
+displaywork();
 projects.display();
+education.display();
 $('#main').append(internationalizeButton);
 $('#mapDiv').append(googleMap);
